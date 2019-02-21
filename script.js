c2bf730cbaba3007d8bdaf6bcc4f0f0ca24db846
Aisling Kathrine <Aisling@Aislings-MacBook-Air.local>
2019-02-21 12:53:53 +0100

    21-02-19 kl. 12.53

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function start() {
     let temp = document.querySelector("template");
 
     async function getJson() {
+        console.log("henter json");
         let jsonData = await fetch("https://mandalskeawebspace.dk/claude_php/clean_up_spreadsheet.php?id=1h8J_KUMMX_texxU-bKC3tNCeTH-ozf47qUHSfinhzJY");
         alleRetter = await jsonData.json();
         visRetter();
@@ -21,9 +22,13 @@ function start() {
             if (filter == "alle" || filter == ret.kategori) {
                 let template = `
                             <div class="ret">
-                                <img src="img/${ret.billede}.png">
                                 <h2>${ret.navn}</h2>
-                                <p>${ret.beskrivelse} <br><br> Pris: ${ret.pris},-</p>
+                                <p>${ret.beskrivelse}</p>
+                                <br>
+                                <h3>Pris: ${ret.pris},-<h3>
+                            </div>
+                            <div class="billede">
+                                <img src="img/${ret.billede}.png">
                             </div>`
                 dest.insertAdjacentHTML("beforeend", template);
                 dest.lastElementChild.addEventListener("click", () => {
@@ -45,7 +50,7 @@ function start() {
     }
 
     document.querySelector("#luk button").addEventListener("click", () => {
-        document.querySelector("#singleview").style.display = "none";
+    document.querySelector("#singleview").style.display = "none";
     })
 
     document.querySelectorAll(".filter").forEach(elm => {
